dff2e37c9de627829a01b06545196baefd8f6810
Your Name <you@example.com>
2024-08-17 18:29:03 +0900

    [change]スタイルの調整

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,18 +6,17 @@ export default function Home() {
   const roomID = randomUUID();
   return (
     <main className="flex min-h-screen flex-col items-center p-24">
-      <h1>
+      <h1 className="relative md:w-[500px] md:h-[500px] w-[350px] h-[350px]">
         <Image
           className="animate-fade-in-top"
           src="/icon/logo.png"
           alt="doccomunication"
-          width="300"
-          height="300"
+          fill
         />
       </h1>
-      <button className="relative inline-flex items-center justify-center p-0.5 mb-2 me-2 overflow-hidden text-sm font-medium text-gray-900 rounded-lg group bg-gradient-to-br from-cyan-500 to-blue-500 group-hover:from-cyan-500 group-hover:to-blue-500 text-[#393d45] hover:text-white focus:ring-4 focus:outline-none focus:ring-cyan-200">
+      <button className="md:static fixed bottom-0 inline-flex items-center justify-center p-0.5 mb-2 md:me-2 overflow-hidden text-sm font-medium text-white md:text-gray-900 rounded-lg group bg-gradient-to-br from-cyan-500 to-blue-500 group-hover:from-cyan-500 group-hover:to-blue-500 hover:text-white focus:ring-4 focus:outline-none focus:ring-cyan-200">
         <Link
-          className="relative px-5 py-2.5 ease-in duration-75 bg-white rounded-md group-hover:bg-opacity-0"
+          className="md:w-40 w-screen px-5 py-2.5 ease-in duration-75 md:bg-white rounded-md md:group-hover:bg-opacity-0"
           href={`/${roomID}`}
         >
           ルームを作る
